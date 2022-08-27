4efcbeff67999cc144137147bba0cfbd7bbe60ac
Tochukwu Bedford <34871260+tochibedford@users.noreply.github.com>
2022-08-27 11:36:09 +0100

    Time: 201 ms (16.89%), Space: 39.8 MB (92.38%) - LeetHub

--- 268-missing-number/268-missing-number.cs
+++ 268-missing-number/268-missing-number.cs
@@ -1,7 +1,7 @@
 public class Solution {
     public int MissingNumber(int[] nums) {
+//      sum of first n nubers in an AP === n/2 × [2a + (n-1)d]
         int sum = ((nums.Length+1)*nums.Length)/2;
-        
         for(int i = 0; i<=nums.Length-1; i++){
             sum -= nums[i];
         }
