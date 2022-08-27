624bf17811db66d922ded4782ff72c9ef2b141a6
Tochukwu Bedford <34871260+tochibedford@users.noreply.github.com>
2022-08-27 10:29:13 +0100

    Time: 107 ms (29.98%), Space: 35 MB (81.05%) - LeetHub

---
+++ 58-length-of-last-word/58-length-of-last-word.cs
@@ -0,0 +1,20 @@
+public class Solution {
+    public int LengthOfLastWord(string s) {
+        int total = 0;
+        int fC = 0;
+        // find first index from behind that's not a space
+        for(int firstCharFromBehindIndex = s.Count()-1; firstCharFromBehindIndex>=0; firstCharFromBehindIndex--){
+            fC = firstCharFromBehindIndex;
+            if(s[firstCharFromBehindIndex] != ' '){
+                break;
+            }
+            
+        }
+        for(int i=fC; i>=0; i--){
+            if(s[i] == ' ')
+                break;
+            total ++;
+        }
+        return total;
+    }
+}
