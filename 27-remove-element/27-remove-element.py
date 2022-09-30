1c0c714342231d018d55a45777d8007c90abfb2c
Tochukwu Bedford <34871260+tochibedford@users.noreply.github.com>
2022-09-30 12:55:27 +0100

    Time: 52 ms (62.16%), Space: 13.8 MB (63.07%) - LeetHub

--- 27-remove-element/27-remove-element.py
+++ 27-remove-element/27-remove-element.py
@@ -1,4 +1,13 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        nums[:] = [i for i in nums if i != val]
-        return len(nums)
+        # nums[:] = [i for i in nums if i != val]
+        # return len(nums)
+        occurences = 0
+        pointer = 0
+        while pointer<len(nums):
+            if nums[pointer] == val:
+                occurences += 1
+            else:
+                nums[pointer - occurences] = nums[pointer]
+            pointer += 1
+        return len(nums) - occurences
