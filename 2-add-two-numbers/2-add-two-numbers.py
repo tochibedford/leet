184ca865f36baf867ac2bb080d2401a94148c523
Tochukwu Bedford <34871260+tochibedford@users.noreply.github.com>
2022-09-30 02:18:00 +0100

    Time: 140 ms (30.81%), Space: 14 MB (43.18%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.py
+++ 2-add-two-numbers/2-add-two-numbers.py
@@ -7,65 +7,22 @@ class ListNode:
     
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        carry = 0
-        result: Union[ListNode, None] = None
-        head: Union[ListNode, None] = None
+        addition = 0
+        result: ListNode = ListNode(0)
+        head: ListNode = result
 
-        while True:
-            addition = l1.val + l2.val + carry
-            if addition >= 10:
-                carry = 1
-                addition = addition - 10
-            else:
-                carry = 0
+        while l1 or l2:
+            if l1:
+                addition += l1.val
+                l1 = l1.next
+            if l2:
+                addition += l2.val
+                l2 = l2.next
+            result.next = ListNode(addition % 10)
+            result = result.next
             
-            if result == None:
-                result = ListNode(addition)
-                head = result
-            else:
-                result.next = ListNode(addition)
-                result = result.next
-            l1 = l1.next
-            l2 = l2.next
-            if not l1 and not l2:
-                if carry:
-                    result.next = ListNode(carry)
-                break
-            if l1 == None:
-                while l2:
-                    addition =  l2.val + carry
-                    if addition >= 10:
-                        carry = 1
-                        addition = addition - 10
-                    else:
-                        carry = 0
-                    if result == None:
-                        result = ListNode(addition)
-                        head = result
-                    else:
-                        result.next = ListNode(addition)
-                        result = result.next
-                    l2 = l2.next
-                if carry:
-                    result.next = ListNode(carry)
-                break
-            elif l2 == None:
-                while l1:
-                    addition =  l1.val + carry
-                    if addition >= 10:
-                        carry = 1
-                        addition = addition - 10
-                    else:
-                        carry = 0
-                    if result == None:
-                        result = ListNode(addition)
-                        head = result
-                    else:
-                        result.next = ListNode(addition)
-                        result = result.next
-                    l1 = l1.next
-                if carry:
-                    result.next = ListNode(carry)
-                break
-        
-        return head
+            addition = 1 if addition > 9 else 0
+
+        if addition:
+            result.next = ListNode(addition)
+        return head.next
